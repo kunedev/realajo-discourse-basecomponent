527a334b068ca19f7580441747bd2254689da9db
kunedev <106591964+kunedev@users.noreply.github.com>
2023-03-28 20:52:25 +0100

    Force update

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 
 # name: kune-discourse-basecomponent
 
-# Extend CSP accepted origins via the Plugin
+# Extend CSP accepted origins via the Plugin 
 extend_content_security_policy(
   script_src: ['https://empowercdn.com/', 'https://kune.co/','https://flow.kune.co/','https://myflow.kune.co/']
 )
